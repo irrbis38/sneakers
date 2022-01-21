758307681e9db87bf51d89c32b67c4c7e62d7854
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-01-21 11:07:16 +0300

    feat: install macro-css, add general styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="wrapper">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="headerLeft d-flex align-center">
+          <img
+            width={40}
+            height={40}
+            src="/img/logo.png"
+            alt="logo"
+            className="mr-15"
+          />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt="user" />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
+    </div>
+  );
 }
 
 export default App;
